918c5eb9b1cfad8f129209e30f44be68dcfcf1d1
Krisanth M <144527593+Krisanth-21@users.noreply.github.com>
2025-09-11 10:05:24 +0530

    Time: 31 ms (58.12%), Space: 30.8 MB (79.4%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,11 +1,11 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        dup = set()
+        dup = set()  # store last values that we've seen last
         for i in range(len(nums)):
             if i > k:
-                dup.remove(nums[i-k-1])
+                dup.remove(nums[i-k-1])  # current - last seen - 1
             
-            if nums[i] in dup:
+            if nums[i] in dup: # If current number already in the set → duplicate found
                 return True
 
             dup.add(nums[i])
